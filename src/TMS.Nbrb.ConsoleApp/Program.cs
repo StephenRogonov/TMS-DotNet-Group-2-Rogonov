6805f04529daafc8b966cf0aa2ee5925f142ae40
julsinkevich <julsinkevich@users.noreply.github.com>
2020-07-24 21:15:39 +0300

    style output for table

--- src/TMS.Nbrb.ConsoleApp/Program.cs
+++ src/TMS.Nbrb.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TMS.Nbrb.Core.Interfaces;
+using TMS.Nbrb.Core.Services;
 
 namespace TMS.Nbrb.ConsoleApp
 {
@@ -6,7 +8,22 @@ namespace TMS.Nbrb.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IRequestService requestService = new RequestService();
+            
+            var data = requestService.GetAllAsync().GetAwaiter().GetResult();
+            Console.WriteLine("_______________________________________________________________________________________");
+            Console.WriteLine("| ID | Abbrevistion |            Currency Name            |     Multilanguage Name     |");
+            Console.WriteLine("_______________________________________________________________________________________");
+
+            foreach (var item in data)
+            {
+                Console.WriteLine("|{0,4}|{1, 14}|{2, 37}|{3, 28}|", item.Cur_ID, item.Cur_Abbreviation, item.Cur_Name, item.Cur_Name_Eng);
+                //Console.WriteLine("|  " + item.Cur_ID+"  | "+item.Cur_Abbreviation +" |  "+ item.Cur_Name + " | " + item.Cur_Name_EngMulti + " |");
+            }
+
+            Console.WriteLine("_______________________________________________________________________________________");
+
+            Console.ReadKey();
         }
     }
 }
